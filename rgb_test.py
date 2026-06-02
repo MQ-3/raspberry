28c35f3e18f63a4c2b2c5a7df1c913cd0f7771c1
yunnjj72 <joyjung040@gmail.com>
2026-06-02 16:49:08 +0900

    try rgb 1

--- rgb_test.py
+++ rgb_test.py
@@ -1,44 +1,55 @@
 import time
 import RPi.GPIO as GPIO
 
-PIN_R = 4   # GCLK
-PIN_G = 3   # SCL
-PIN_B = 2   # SDA
+PIN_R = 4  # GCLK
+PIN_G = 3  # SCL
+PIN_B = 2  # SDA
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup([PIN_R, PIN_G, PIN_B], GPIO.OUT)
 
+pwm_r = GPIO.PWM(PIN_R, 100)
+pwm_g = GPIO.PWM(PIN_G, 100)
+pwm_b = GPIO.PWM(PIN_B, 100)
 
-def all_off_high():
-    GPIO.output([PIN_R, PIN_G, PIN_B], GPIO.HIGH)
+pwm_r.start(0)
+pwm_g.start(0)
+pwm_b.start(0)
 
 
-def all_off_low():
-    GPIO.output([PIN_R, PIN_G, PIN_B], GPIO.LOW)
+def set_color(r, g, b):
+    pwm_r.ChangeDutyCycle(r / 255 * 100)
+    pwm_g.ChangeDutyCycle(g / 255 * 100)
+    pwm_b.ChangeDutyCycle(b / 255 * 100)
 
 
 try:
-    print("=== 방식 1: 공통 캐소드 (HIGH = ON) ===")
-    all_off_low()
-    for name, pin in [("R(빨강)", PIN_R), ("G(초록)", PIN_G), ("B(파랑)", PIN_B)]:
-        print(f"{name} 켜는 중...")
-        GPIO.output(pin, GPIO.HIGH)
-        time.sleep(2)
-        GPIO.output(pin, GPIO.LOW)
+    print("빨강")
+    set_color(255, 0, 0)
+    time.sleep(2)
 
-    print("\n=== 방식 2: 공통 애노드 (LOW = ON) ===")
-    all_off_high()
-    for name, pin in [("R(빨강)", PIN_R), ("G(초록)", PIN_G), ("B(파랑)", PIN_B)]:
-        print(f"{name} 켜는 중...")
-        GPIO.output(pin, GPIO.LOW)
-        time.sleep(2)
-        GPIO.output(pin, GPIO.HIGH)
+    print("초록")
+    set_color(0, 255, 0)
+    time.sleep(2)
+
+    print("파랑")
+    set_color(0, 0, 255)
+    time.sleep(2)
+
+    print("흰색")
+    set_color(255, 255, 255)
+    time.sleep(2)
+
+    print("끔")
+    set_color(0, 0, 0)
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    all_off_high()
+    pwm_r.stop()
+    pwm_g.stop()
+    pwm_b.stop()
     GPIO.cleanup()
-    print("\n종료")
+    print("종료")
