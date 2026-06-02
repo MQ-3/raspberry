10ea6612323fabb97b37208465fe4be93cc5aba9
yunnjj72 <joyjung040@gmail.com>
2026-06-02 16:58:30 +0900

    try rgb 3

--- rgb_test.py
+++ rgb_test.py
@@ -1,30 +1,14 @@
 import time
-import RPi.GPIO as GPIO
+from gpiozero import RGBLED
 
-PIN_R = 4  # GCLK
-PIN_G = 3  # SCL
-PIN_B = 2  # SDA
+BRIGHTNESS = 0.3  # 밝기 0.0~1.0
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup([PIN_R, PIN_G, PIN_B], GPIO.OUT)
-
-pwm_r = GPIO.PWM(PIN_R, 100)
-pwm_g = GPIO.PWM(PIN_G, 100)
-pwm_b = GPIO.PWM(PIN_B, 100)
-
-pwm_r.start(0)
-pwm_g.start(0)
-pwm_b.start(0)
+led = RGBLED(red=4, green=3, blue=2, active_high=True)
 
 
 def set_color(r, g, b):
-    pwm_r.ChangeDutyCycle(r / 255 * BRIGHTNESS)
-    pwm_g.ChangeDutyCycle(g / 255 * BRIGHTNESS)
-    pwm_b.ChangeDutyCycle(b / 255 * BRIGHTNESS)
+    led.color = (r / 255 * BRIGHTNESS, g / 255 * BRIGHTNESS, b / 255 * BRIGHTNESS)
 
-
-BRIGHTNESS = 30  # 밝기 0~100
 
 try:
     print("빨강")
@@ -44,16 +28,12 @@ try:
     time.sleep(2)
 
     print("끔")
-    set_color(0, 0, 0)
+    led.off()
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    set_color(0, 0, 0)
-    pwm_r.stop()
-    pwm_g.stop()
-    pwm_b.stop()
-    GPIO.output([PIN_R, PIN_G, PIN_B], GPIO.LOW)
-    GPIO.cleanup()
+    led.off()
+    led.close()
     print("종료")
